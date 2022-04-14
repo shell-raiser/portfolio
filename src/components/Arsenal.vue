8880fcd9c5faf4dd4418253d32cb3231983ae8fc
shell-raiser <ksshailesh05@gmail.com>
2022-04-14 21:19:58 +0530

    added init icons

--- src/components/Arsenal.vue
+++ src/components/Arsenal.vue
@@ -1,6 +1,16 @@
 <template>
-<section>
-<h1> Arsenal </h1>
-    <p>coming soon</p>
+    <section>
+        <h1> Arsenal </h1>
+        <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png" height="100">
+        <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png" height="100">
+        <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/java/java.png" height="100">
+        <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/cpp/cpp.png" height="100">
+        <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png" height="100">
+        <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/c/c.png" height="100">
+        <img src="https://pbs.twimg.com/profile_images/1327348581372575744/6M3Ll1hq_400x400.jpg" height="100">
+        <img src="https://pbs.twimg.com/profile_images/1036953347943743488/1dbRCWDq_400x400.jpg" height="100">
+        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Canva_Logo.png/1920px-Canva_Logo.png" height="100">
+        <img src="https://nodejs.org/static/images/logos/nodejs-new-pantone-black.svg" height="100">
+            
     </section>
 </template>
