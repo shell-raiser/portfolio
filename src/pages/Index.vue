da548a57a165ac030aac16d3b3433007717b795d
shell-raiser <ksshailesh05@gmail.com>
2022-04-15 22:49:57 +0530

    removed useless code

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -106,21 +106,21 @@ export default {
   data: () => ({
     overlay: false,
     // sections: { <About /> ,<Projects/>, <Contact/> },
-    onboarding: 0,
+    // onboarding: 0,
 
   }),
-  methods: {
-    next() {
-      this.onboarding = this.onboarding + 1 === this.length
-        ? 0
-        : this.onboarding + 1
-    },
-    prev() {
-      this.onboarding = this.onboarding - 1 < 0
-        ? this.length - 1
-        : this.onboarding - 1
-    },
-  },
+  // methods: {
+  //   next() {
+  //     this.onboarding = this.onboarding + 1 === this.length
+  //       ? 0
+  //       : this.onboarding + 1
+  //   },
+  //   prev() {
+  //     this.onboarding = this.onboarding - 1 < 0
+  //       ? this.length - 1
+  //       : this.onboarding - 1
+  //   },
+  // },
 
 
 };
