ef9f5e565800f62a31a1e83c5919a317a3339706
shell-raiser <ksshailesh05@gmail.com>
2022-04-15 22:48:05 +0530

    added dosha quiz and DGS game to project list

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,6 +1,13 @@
 <template>
-<section>
-<h1> Projects </h1>
-    <p>Nothing yet, coming soon</p>
+    <section>
+        <h1> Projects </h1>
+        <v-card>
+            <v-list>
+                <v-list-item-group>
+                    <v-list-item>https://github.com/shell-raiser/Dosha-quiz-app</v-list-item>
+                    <v-list-item>https://github.com/shell-raiser/Dark-Green-Space</v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-card>
     </section>
 </template>
