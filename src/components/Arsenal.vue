1324093b2be714fde080bc5904cbcb38325558ec
shell-raiser <ksshailesh05@gmail.com>
2022-04-24 13:14:07 +0530

    added more tools

--- src/components/Arsenal.vue
+++ src/components/Arsenal.vue
@@ -1,35 +1,45 @@
 <template>
     <section>
         <h1> Arsenal </h1>
-        <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png" height="100">
-        
-        <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png" height="100">
-        
-        <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/java/java.png" height="100">
-        
-        <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/cpp/cpp.png" height="100">
-        
-        <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png" height="100">
-        
-        <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/c/c.png" height="100">
-        
-        <img src="https://pbs.twimg.com/profile_images/1327348581372575744/6M3Ll1hq_400x400.jpg" height="100">
-        
-        <img src="https://pbs.twimg.com/profile_images/1036953347943743488/1dbRCWDq_400x400.jpg" height="100">
-        
-        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Canva_Logo.png/1920px-Canva_Logo.png"
-            height="100">
-        
-        <img src="https://nodejs.org/static/images/logos/nodejs-new-pantone-black.svg" height="100">
-        
-        <img src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-dark.svg" height="100">
-        
-        <img src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg" height="100">
-        
+        <v-list>
+            <v-card class="mx-auto">
+                <v-list-item-content>
+                    <v-list-item-title>Langs</v-list-item-title>
+                    
+                    <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png"
+                        height="100">
 
-        <svg viewBox="0 10 127 110" width="110" height="100">
-            <path fill="#42b883" d="M78.8,10L64,35.4L49.2,10H0l64,110l64-110C128,10,78.8,10,78.8,10z"></path>
-            <path fill="#35495e" d="M78.8,10L64,35.4L49.2,10H25.6L64,76l38.4-66H78.8z"></path>
-        </svg>
+                    <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png"
+                        height="100">
+                </v-list-item-content>
+            </v-card>
+
+            <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/java/java.png" height="100">
+
+            <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/cpp/cpp.png" height="100">
+
+            <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png" height="100">
+
+            <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/c/c.png" height="100">
+
+            <img src="https://pbs.twimg.com/profile_images/1327348581372575744/6M3Ll1hq_400x400.jpg" height="100">
+
+            <img src="https://pbs.twimg.com/profile_images/1036953347943743488/1dbRCWDq_400x400.jpg" height="100">
+
+            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Canva_Logo.png/1920px-Canva_Logo.png"
+                height="100">
+
+            <img src="https://nodejs.org/static/images/logos/nodejs-new-pantone-black.svg" height="100">
+
+            <img src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-dark.svg" height="100">
+
+            <img src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg" height="100">
+
+
+            <svg viewBox="0 10 127 110" width="110" height="100">
+                <path fill="#42b883" d="M78.8,10L64,35.4L49.2,10H0l64,110l64-110C128,10,78.8,10,78.8,10z"></path>
+                <path fill="#35495e" d="M78.8,10L64,35.4L49.2,10H25.6L64,76l38.4-66H78.8z"></path>
+            </svg>
+        </v-list>
     </section>
 </template>
