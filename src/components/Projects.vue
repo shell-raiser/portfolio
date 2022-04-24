c547efb5b0b07e954dca9c4367b7bf6fa7f8c487
shell-raiser <ksshailesh05@gmail.com>
2022-04-24 13:14:31 +0530

    added bg to projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,13 +1,15 @@
 <template>
     <section>
-        <h1> Projects </h1>
-        <v-card>
-            <v-list>
-                <v-list-item-group>
-                    <v-list-item>https://github.com/shell-raiser/Dosha-quiz-app</v-list-item>
-                    <v-list-item>https://github.com/shell-raiser/Dark-Green-Space</v-list-item>
-                </v-list-item-group>
-            </v-list>
-        </v-card>
+        <v-container class="bg3">
+            <h1> Projects </h1>
+            <v-card>
+                <v-list>
+                    <v-list-item-group>
+                        <v-list-item>https://github.com/shell-raiser/Dosha-quiz-app</v-list-item>
+                        <v-list-item>https://github.com/shell-raiser/Dark-Green-Space</v-list-item>
+                    </v-list-item-group>
+                </v-list>
+            </v-card>
+        </v-container>
     </section>
 </template>
