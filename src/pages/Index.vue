4018e2a119ab275676994cf34eff1f072c0dd3f1
shell-raiser <ksshailesh05@gmail.com>
2022-04-24 16:43:45 +0530

    window improve-added footer instead of card action

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,58 +2,71 @@
   <Layout>
     <!-- <div> -->
     <v-card dark flat tile>
-      <v-window v-model="onboarding">
-        <v-window-item  >
-          <v-card color="transparent" height="100%">
+      <v-window v-model="onboarding" vertical>
+        <v-window-item>
+          <!-- <v-card style="bottom:0px;"> -->
             <!-- <v-row class="fill-height" align="center" justify="center"> -->
-                <About id="About" />
+            <About id="About" />
+            <!-- </v-row> -->
+          <!-- </v-card> -->
+        </v-window-item>
+
+        <v-window-item>
+          <v-card>
+            <!-- <v-row class="fill-height" align="center" justify="center"> -->
+            <Contact id="Contact" />
             <!-- </v-row> -->
           </v-card>
         </v-window-item>
         <v-window-item>
-          <v-card color="transparent" height="100%">
-            <!-- <v-row class="fill-height" align="center" justify="center"> -->
-              <v-card-text class="text-center">
-                <Contact id="Contact" />
-              </v-card-text>
-            <!-- </v-row> -->
-          </v-card>
+          <!-- <v-card> -->
+            <v-row class="fill-height" align="center" justify="center">
+              <Projects id="Projects" />
+            </v-row>
+          <!-- </v-card> -->
         </v-window-item>
         <v-window-item>
-          <v-card color="transparent" height="100%">
+          <!-- <v-card> -->
             <v-row class="fill-height" align="center" justify="center">
-              <v-card-text class="text-center">
-                <Projects id="Projects" />
-              </v-card-text>
+              <Arsenal id="Arsenal" />
             </v-row>
-          </v-card>
-        </v-window-item>
-        <v-window-item>
-          <v-card color="transparent" height="100%">
-            <v-row class="fill-height" align="center" justify="center">
-              <v-card-text class="text-center">
-                <Arsenal id="Arsenal" />
-              </v-card-text>
-            </v-row>
-          </v-card>
+          <!-- </v-card> -->
         </v-window-item>
       </v-window>
 
-      <v-card-actions class="justify-space-between">
+      <!-- <v-card-actions style="position:fixed bottom: 0;" class="justify-space-between "> -->
+        <v-footer class="justify-space-between " tile app>
         <v-btn text @click="prev">
           <v-icon>mdi-chevron-left</v-icon>
         </v-btn>
         <v-item-group v-model="onboarding" class="text-center" mandatory>
-          <v-item v-for="n in length" :key="`btn-${n}`" v-slot="{ active, toggle }">
+          <v-item v-slot="{ active, toggle }">
             <v-btn :input-value="active" icon @click="toggle">
-              <v-icon>mdi-record</v-icon>
+              <v-icon>mdi-view-dashboard</v-icon>
             </v-btn>
           </v-item>
+          <v-item v-slot="{ active, toggle }">
+            <v-btn :input-value="active" icon @click="toggle">
+              <v-icon>mdi-human-greeting-proximity</v-icon>
+            </v-btn>
+          </v-item>
+          <v-item v-slot="{ active, toggle }">
+            <v-btn :input-value="active" icon @click="toggle">
+              <v-icon>mdi-devices</v-icon>
+            </v-btn>
+          </v-item>
+          <v-item v-slot="{ active, toggle }">
+            <v-btn :input-value="active" icon @click="toggle">
+              <v-icon>mdi-hammer-wrench</v-icon>
+            </v-btn>
+          </v-item>
+
         </v-item-group>
         <v-btn text @click="next">
           <v-icon>mdi-chevron-right</v-icon>
         </v-btn>
-      </v-card-actions>
+        </v-footer>
+      <!-- </v-card-actions> -->
     </v-card>
   </Layout>
 </template>
@@ -64,7 +77,6 @@ import Contact from "@/components/Contact.vue";
 import Projects from "@/components/Projects.vue";
 import 'animate.css';
 import Arsenal from "../components/Arsenal.vue";
-import Test from "../components/test.vue";
 export default {
   metaInfo: {
     title: "Shailesh K S",
@@ -74,29 +86,29 @@ export default {
     Contact,
     Projects,
     Arsenal,
-    Test
   },
 
   data: () => ({
     overlay: false,
     length: 4,
-      onboarding: 0,
+    onboarding: 0,
+    window: 0
     // sections: { <About /> ,<Projects/>, <Contact/> },
     // onboarding: 0,
 
   }),
   methods: {
-      next () {
-        this.onboarding = this.onboarding + 1 === this.length
-          ? 0
-          : this.onboarding + 1
-      },
-      prev () {
-        this.onboarding = this.onboarding - 1 < 0
-          ? this.length - 1
-          : this.onboarding - 1
-      },
-    }
+    next() {
+      this.onboarding = this.onboarding + 1 === this.length
+        ? 0
+        : this.onboarding + 1
+    },
+    prev() {
+      this.onboarding = this.onboarding - 1 < 0
+        ? this.length - 1
+        : this.onboarding - 1
+    },
+  }
   // methods: {
   //   next() {
   //     this.onboarding = this.onboarding + 1 === this.length
@@ -115,7 +127,4 @@ export default {
 </script>
 
 <style>
-.home-links a {
-  margin-right: 1rem;
-}
 </style>
