0031f01f4e48f7353c2db4be74198083b808485d
shell-raiser <ksshailesh05@gmail.com>
2022-04-25 17:59:10 +0530

    formatting

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,53 +1,54 @@
 <template>
-    <section  class="bg2">
+    <section class="bg2">
         <v-container>
-        <h1>Contact</h1>
-        <v-card>
-            <v-list>
-                <v-list-item-group>
-                    <v-list-item two-line>
-                    <v-list-item-icon>
-                        <v-icon> mdi-email-variant</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title>Email at</v-list-item-title>
-                        <v-list-item-subtitle>someemail@gmail.com</v-list-item-subtitle>
-                    </v-list-item-content>
-                    </v-list-item>
-                    
-                    <v-list-item two-line>
-                    <v-list-item-icon>
-                        <v-icon>mdi-github</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title>Github</v-list-item-title>
-                        <v-list-item-subtitle>@shell-raiser</v-list-item-subtitle>
-                    </v-list-item-content>
-                    </v-list-item>
+            <h1>Contact</h1>
+            <v-card>
+                <!-- <v-list> -->
+                    <v-list-item-group>
+                        <v-list-item two-line>
+                            <v-list-item-icon>
+                                <v-icon> mdi-email-variant</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-content>
+                                <v-list-item-title>Email at</v-list-item-title>
+                                <v-list-item-subtitle>someemail@gmail.com</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
 
-                    <v-list-item two-line>
-                    <v-list-item-icon>
-                        <v-icon>mdi-soundcloud</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title>Soundcloud</v-list-item-title>
-                        <v-list-item-subtitle>soundcloud.com/user-185450671</v-list-item-subtitle>
-                    </v-list-item-content>
-                    </v-list-item>
+                        <v-list-item two-line>
+                            <v-list-item-icon>
+                                <v-icon>mdi-github</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-content>
+                                <v-list-item-title>Github</v-list-item-title>
+                                <v-list-item-subtitle>@shell-raiser</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
 
-                    <v-list-item two-line>
-                    <v-list-item-icon>
-                        <v-icon>mdi-youtube</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title>Youtube</v-list-item-title>
-                        <v-list-item-subtitle>youtube.com/channel/UCmEShK3aru1usVrGOrzGqqw</v-list-item-subtitle>
-                    </v-list-item-content>
-                    </v-list-item>
-                </v-list-item-group>
+                        <v-list-item two-line>
+                            <v-list-item-icon>
+                                <v-icon>mdi-soundcloud</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-content>
+                                <v-list-item-title>Soundcloud</v-list-item-title>
+                                <v-list-item-subtitle>soundcloud.com/user-185450671</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
 
-            </v-list>
-        </v-card>
+                        <v-list-item two-line>
+                            <v-list-item-icon>
+                                <v-icon>mdi-youtube</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-content>
+                                <v-list-item-title>Youtube</v-list-item-title>
+                                <v-list-item-subtitle>youtube.com/channel/UCmEShK3aru1usVrGOrzGqqw
+                                </v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list-item-group>
+
+                <!-- </v-list> -->
+            </v-card>
         </v-container>
     </section>
 </template>
