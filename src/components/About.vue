841d10843040c91a9d8c894310df520e66d5a6cc
shell-raiser <ksshailesh05@gmail.com>
2022-05-05 14:48:19 +0530

    formatting

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,14 +5,19 @@
 
       <v-row>
         <v-col sm="6">
-          <v-card  hover raised rounded :class="{ 'on-hover': hover }" class="mx-auto" max-width="400" max-height="400">
+          <v-card hover raised rounded :class="{ 'on-hover': hover }" class="mx-auto">
             <!-- <g-image   alt="Profile Picture" src="~/profilePic.jpg" max-width="400" max-height="400" fit="cover" /> -->
             <v-img src="https://randomuser.me/api/portraits/lego/5.jpg"></v-img>
           </v-card>
         </v-col>
         <v-col sm="6">
-          <h1>Yollo there, I'm Shailesh</h1>
-          <p style="color:black">I'm a CyberSec enthusiast</p>
+          <h1>Yollo there,
+            <br>
+            I'm Shailesh
+          </h1>
+          <p style="color:black">
+            I'm a CyberSec enthusiast
+          </p>
         </v-col>
       </v-row>
     </v-container>
