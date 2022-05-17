7c0b2ff9eee8818a3defe982a140831ec0d8874b
shell-raiser <ksshailesh05@gmail.com>
2022-05-17 20:44:12 +0530

    added pebbleanimation to projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,14 +2,21 @@
     <section class="bg3">
         <v-container>
             <h1 class="ProjectsHead"> Projects </h1>
-            <v-card>
-                <v-list>
-                    <v-list-item-group>
-                        <v-list-item>https://github.com/shell-raiser/Dosha-quiz-app</v-list-item>
-                        <v-list-item>https://github.com/shell-raiser/Dark-Green-Space</v-list-item>
-                    </v-list-item-group>
-                </v-list>
-            </v-card>
+            <v-row>
+                <v-col sm="6">
+                    <v-card>
+                        <!-- <v-list> -->
+                        <v-list-item-group>
+                            <v-list-item>https://github.com/shell-raiser/Dosha-quiz-app</v-list-item>
+                            <v-list-item>https://github.com/shell-raiser/Dark-Green-Space</v-list-item>
+                        </v-list-item-group>
+                        <!-- </v-list> -->
+                    </v-card>
+                </v-col>
+                <v-col sm="6">
+                    <img src="../pebbleAnimationC.gif" style=" display: block; margin-left: auto; margin-right: auto; height: 50%; pointer-events: none; overflow: hidden;" />
+                </v-col>
+            </v-row>
         </v-container>
     </section>
 </template>
