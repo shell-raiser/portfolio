f186b3ffc7a54262a62576f9f3f207eac212117c
shell-raiser <ksshailesh05@gmail.com>
2022-05-26 18:05:24 +0530

    gif bg for projects, right aligned all content

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,22 +1,30 @@
 <template>
     <section class="bg3">
-        <v-container>
-            <h1 class="ProjectsHead"> Projects </h1>
+        <v-container style="z-index:10;">
+            
             <v-row>
+                <v-col sm="6" class="d-none d-sm-block">
+                    
+                </v-col>
                 <v-col sm="6">
-                    <v-card>
+                    <h1 class="ProjectsHead"> Projects </h1>
+                    <v-card shaped>
                         <!-- <v-list> -->
                         <v-list-item-group>
-                            <v-list-item>https://github.com/shell-raiser/Dosha-quiz-app</v-list-item>
-                            <v-list-item>https://github.com/shell-raiser/Dark-Green-Space</v-list-item>
+                            <v-list-item href="https://github.com/shell-raiser/Dosha-quiz-app" target="_blank"
+                                rel="noopener noreferrer">https://github.com/shell-raiser/Dosha-quiz-app
+                            </v-list-item>
+                            <v-list-item href="https://github.com/shell-raiser/Dark-Green-Space" target="_blank"
+                                rel="noopener noreferrer">https://github.com/shell-raiser/Dark-Green-Space
+                            </v-list-item>
                         </v-list-item-group>
                         <!-- </v-list> -->
                     </v-card>
                 </v-col>
-                <v-col sm="6">
-                    <img src="../pebbleAnimationC.gif" style=" display: block; margin-left: auto; margin-right: auto; height: 50%; pointer-events: none; overflow: hidden;" />
-                </v-col>
             </v-row>
+
+
+
         </v-container>
     </section>
 </template>
