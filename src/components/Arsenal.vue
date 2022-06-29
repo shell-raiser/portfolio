3337bbe1ed9dce18d26a8aaaaa06a41471e11c2b
Shell Raiser <ksshailesh05@gmail.com>
2022-06-29 20:06:58 +0530

    made heading out of container. Change BG to pic

--- src/components/Arsenal.vue
+++ src/components/Arsenal.vue
@@ -1,47 +1,51 @@
 <template>
     <section class="bg4">
+            <h1 class="ArsenalHead rounded text-center">Arsenal</h1>
         <v-container>
-            <h1 class="ArsenalHead">Arsenal</h1>
-            <v-card class="white" rounded flat >
-            <!-- <v-list> -->
-            <!-- <v-list-item-content> -->
-            
-            <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png" height="100" />
 
-            <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png"
-                height="100" />
-            <!-- </v-list-item-content> -->
-            <!-- </v-card> -->
+            <v-card class="white" rounded flat>
+                <!-- <v-list> -->
+                <!-- <v-list-item-content> -->
 
-            <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/java/java.png" height="100" />
+                <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png"
+                    height="100" />
 
-            <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/cpp/cpp.png" height="100" />
+                <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png"
+                    height="100" />
+                <!-- </v-list-item-content> -->
+                <!-- </v-card> -->
 
-            <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png" height="100" />
+                <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/java/java.png" height="100" />
 
-            <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/c/c.png" height="100" />
+                <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/cpp/cpp.png" height="100" />
 
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Qiskit-Logo.svg/800px-Qiskit-Logo.svg.png" height="100" />
-            
+                <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png" height="100" />
 
-            <img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Logo_Blender.svg" height="100" />
+                <img src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/c/c.png" height="100" />
 
-            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Canva_Logo.png/1920px-Canva_Logo.png"
-                height="100" />
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Qiskit-Logo.svg/800px-Qiskit-Logo.svg.png"
+                    height="100" />
 
-            <img src="https://nodejs.org/static/images/logos/nodejs-new-pantone-black.svg" height="100" />
 
-            <img src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-dark.svg" height="100" />
+                <img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Logo_Blender.svg" height="100" />
 
-            <img src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg" height="100" />
+                <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Canva_Logo.png/1920px-Canva_Logo.png"
+                    height="100" />
 
-            <svg viewBox="0 10 127 110" width="110" height="100">
-                <path fill="#42b883" d="M78.8,10L64,35.4L49.2,10H0l64,110l64-110C128,10,78.8,10,78.8,10z"></path>
-                <path fill="#35495e" d="M78.8,10L64,35.4L49.2,10H25.6L64,76l38.4-66H78.8z"></path>
-            </svg>
+                <img src="https://nodejs.org/static/images/logos/nodejs-new-pantone-black.svg" height="100" />
 
-            <img src="https://webimages.mongodb.com/_com_assets/cms/kuyjf3vea2hg34taa-horizontal_default_slate_blue.svg" alt="mongodb" height="100" />
-            <!-- </v-list> -->
+                <img src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-dark.svg" height="100" />
+
+                <img src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg" height="100" />
+
+                <svg viewBox="0 10 127 110" width="110" height="100">
+                    <path fill="#42b883" d="M78.8,10L64,35.4L49.2,10H0l64,110l64-110C128,10,78.8,10,78.8,10z"></path>
+                    <path fill="#35495e" d="M78.8,10L64,35.4L49.2,10H25.6L64,76l38.4-66H78.8z"></path>
+                </svg>
+
+                <img src="https://webimages.mongodb.com/_com_assets/cms/kuyjf3vea2hg34taa-horizontal_default_slate_blue.svg"
+                    alt="mongodb" height="100" />
+                <!-- </v-list> -->
             </v-card>
         </v-container>
     </section>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -115,8 +115,12 @@ background: lightblue url("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.c
 
 .bg4 {
   min-height: 95vh;
-  background: linear-gradient(-45deg, #09254f, #23d5ab);
+  /* background: linear-gradient(-45deg, #09254f, #23d5ab);
   background-size: 200% 200%;
-  animation: gradient 10s ease infinite;
+  animation: gradient 10s ease infinite; */
+  background: lightblue url("https://images.unsplash.com/photo-1655910028222-4abc2d90edcc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"); 
+  /* filter: blur(8px); */
+  /* -webkit-filter: blur(8px); */
+  background-size: cover;
 }
 </style>
