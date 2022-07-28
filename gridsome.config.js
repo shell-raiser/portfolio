ba276d173a1a58b1223817206d59c6ea75430fef
Shell Raiser <ksshailesh05@gmail.com>
2022-07-28 20:20:13 +0530

    add site icon

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
+  icon: './src/Shell.svg',
   siteName: 'Portfolio',
   plugins: [],
   siteUrl: 'https://shell-raiser.github.io'
