56137dfd93d6b8d633c99fed57b09984aa0063e8
Shell Raiser <ksshailesh05@gmail.com>
2022-09-20 21:13:01 +0530

    add chips in nav bar

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -4,32 +4,33 @@
 
 		<!-- </v-card> -->
 		<v-navigation-drawer id="theNavbar" app fixed v-if="this.$router.history.current['path'] == '/projects'" right>
-			<template v-slot:prepend>
-				<v-list-item two-line>
-					<v-list-item-avatar>
-						<img src="https://randomuser.me/api/portraits/women/81.jpg">
-					</v-list-item-avatar>
-
-					<v-list-item-content>
-						<v-list-item-title>Jane Smith</v-list-item-title>
-						<v-list-item-subtitle>Logged In</v-list-item-subtitle>
-					</v-list-item-content>
-				</v-list-item>
-			</template>
+			<h3>Projects and Experience</h3>
 
 			<v-divider></v-divider>
 
-			<v-list dense>
-				<v-list-item v-for="item in items" :key="item.title">
-					<v-list-item-icon>
-						<v-icon>{{ item.icon }}</v-icon>
-					</v-list-item-icon>
+			<v-chip-group column multiple>
+				<v-chip active-class="deep-purple--text">
+					Python
+				</v-chip>
+				<v-chip active-class="deep-purple--text">
+					Javascript
+				</v-chip>
+				<v-chip active-class="deep-purple--text">
+					Java
+				</v-chip>
+				<v-chip active-class="deep-purple--text">
+					C++
+				</v-chip>
+				<v-chip active-class="deep-purple--text">
+					SQL
+				</v-chip>
+				<v-chip active-class="deep-purple--text">
+					Qiskit
+				</v-chip>
+			</v-chip-group>
 
-					<v-list-item-content>
-						<v-list-item-title>{{ item.title }}</v-list-item-title>
-					</v-list-item-content>
-				</v-list-item>
-			</v-list>
+
+
 		</v-navigation-drawer>
 		<main>
 			<v-main>
