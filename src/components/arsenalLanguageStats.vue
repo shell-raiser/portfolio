33c30268d77241480cc74b76fb212e83e7e8ad49
Shell Raiser <ksshailesh05@gmail.com>
2023-04-02 22:26:34 +0530

    add width limit to langstats

--- src/components/arsenalLanguageStats.vue
+++ src/components/arsenalLanguageStats.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-container class="rounded" style="backdrop-filter:blur(5px); background: rgba(209, 196, 233, 0.5);">
+    <v-container class="rounded" style="backdrop-filter:blur(5px); background: rgba(209, 196, 233, 0.5); width:fit-content">
         <g-image style="max-width: 100%"
             src="https://github-readme-stats.vercel.app/api?username=shell-raiser&show_icons=true&title_color=200254&bg_color=0,90caf9,9575CD&text_color=0e0126&count_private=true&custom_title=My%20Github%20Stats&hide_rank=true" />
         <br>
