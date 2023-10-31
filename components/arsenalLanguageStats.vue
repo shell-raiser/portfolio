6837ac42a081ffff8b3caf9614a97e585509b8f1
Shell Raiser <ksshailesh05@gmail.com>
2023-10-31 10:52:02 +0530

    remove lang stats img

--- components/arsenalLanguageStats.vue
+++ components/arsenalLanguageStats.vue
@@ -3,9 +3,9 @@
         <img style="max-width: 100%"
             src="https://github-readme-stats.vercel.app/api?username=shell-raiser&show_icons=true&title_color=200254&bg_color=0,90caf9,9575CD&text_color=0e0126&count_private=true&custom_title=My%20Github%20Stats&hide_rank=true" />
         <br>
-        <img style="max-width: 100%"
+        <!-- <img style="max-width: 100%"
             src="https://github-readme-stats.vercel.app/api/top-langs/?username=shell-raiser&layout=default&title_color=200254&bg_color=0,90caf9,9575CD&text_color=0e0126&count_private=true&exclude_repo=Qiskit-Developer-Certification-Notes-and-Code,REVA-University-CSE-Code,codefury5.0" />
-        <br>
+        <br> -->
         <a href="https://tryhackme.com/p/shellRaiser" target="_blank">
             <img src="https://tryhackme-badges.s3.amazonaws.com/shellRaiser.png" alt="TryHackMe">
         </a>
