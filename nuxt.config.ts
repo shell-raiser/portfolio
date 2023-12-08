a731349982837a7353b7c1ae9b62c3a23d3e39ab
shell-raiser <ksshailesh05@gmail.com>
2023-12-08 14:37:15 +0000

    set ssg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 export default defineNuxtConfig({
+  ssr: false,
   css: [
     "vuetify/lib/styles/main.sass",
     "@mdi/font/css/materialdesignicons.min.css",
