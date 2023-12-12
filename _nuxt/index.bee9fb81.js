efe24819c088d82b0fcc0ec677cd5b81e977758c
shell-raiser <shell-raiser@users.noreply.github.com>
2023-12-12 12:11:52 +0000

    deploy: 4df3a3c7caea6b26122848e1784158a5c88b35e8

--- _nuxt/index.bee9fb81.js
+++
@@ -1,1 +0,0 @@
-import{S as t}from"./entry.c071ac25.js";const i=t("user",{state:()=>({chipSelected:[],activeSection:"about"}),actions:{changeActiveSection(e){this.activeSection=e}}});export{i as u};
