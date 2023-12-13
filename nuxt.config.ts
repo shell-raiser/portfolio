3e8ca880ee848387b41e3e22cc9f0655acff9338
shell-raiser <ksshailesh05@gmail.com>
2023-12-13 10:27:22 +0000

    set ssr back to true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   css: [
     "vuetify/lib/styles/main.sass",
     "@mdi/font/css/materialdesignicons.min.css",
